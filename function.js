64f7945f9644aaac28a08dcc737420375d987c7b
Drop-G <tedpetersiv@gmail.com>
2021-01-27 22:08:32 -0600

    console.log for fullName

--- function.js
+++ function.js
@@ -1,15 +1,4 @@
-// Enter your function between the lines and run  --->  $node function.js <--- In the terminal
+// Enter your function between the lines and run  ---> node function.js <--- In the terminal
 // *****************************************************************************************
-console.log('******FUNCTIONS********')
-        var myFunc=function(param1,param2){
-            var sum=param1+param2
-            console.log(sum)
-            //3
-            //12
-            //3 , 3   undefined 
-        }
-        myFunc(1,2)
-        myFunc(1,'2')
-        myFunc(1,2,3)
 
 // *****************************************************************************************

205ea3dff36c1680b791efb24df940045a89e7c6
Drop-G <tedpetersiv@gmail.com>
2021-02-23 17:57:56 -0600

    Objects, Prototype Object Patterns, and String Manipulation

--- function.js
+++ function.js
@@ -1,4 +1,13 @@
 // Enter your function between the lines and run  ---> node function.js <--- In the terminal
 // *****************************************************************************************
+function formatGreeting(name, city){
+    var retString = ""
+    retString += "Hello " + name + ", ";
+    retString += "welcome to " + city + "!";
+ return retString;
+}
 
+var greeting = formatGreeting("DickCheese", "Rome")
+console.log(greeting)
+console.log(greeting)
 // *****************************************************************************************
